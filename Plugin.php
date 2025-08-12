54d45b9794b5f236045ac52b28a22910babd223c
QingYun <125800889+ThisIsQingYun@users.noreply.github.com>
2025-08-12 11:11:13 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -6,7 +6,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  * 
  * @package PopupNotice
  * @author QingYun
- * @version 1.0.0
+ * @version 1.1.0
  * @link https://www.dogecloud.com/
  */
 class PopupNotice_Plugin implements Typecho_Plugin_Interface
